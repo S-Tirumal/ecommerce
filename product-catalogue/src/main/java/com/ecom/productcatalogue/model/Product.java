01a0d3438a2335c5e2dd9a14890ae6ee23f2616c
sutrave.tirumal@gmail.com <sutrave.tirumal@gmail.com>
2025-06-29 15:31:09 +0530

    Added the getter setter to Product

--- product-catalogue/src/main/java/com/ecom/productcatalogue/model/Product.java
+++ product-catalogue/src/main/java/com/ecom/productcatalogue/model/Product.java
@@ -2,9 +2,13 @@ package com.ecom.productcatalogue.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.IdGeneratorType;
 
 @Entity
+@Getter
+@Setter
 public class Product extends BaseModel{
     private String name;
     private Double price;
